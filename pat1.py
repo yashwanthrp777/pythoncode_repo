59179f6d1bcdec2f865f3bb496082ae4286e1024
Yashwanth R P <yyashwanthrp@gmail.com>
2026-05-17 23:47:53 +0530

    2nd commit

---
+++ pat1.py
@@ -0,0 +1,18 @@
+def pat(n):
+    for i in range(n):
+        for j in range(i,n):
+            print(" ",end=" ")
+        for j in range(i+1):
+            print("$",end=" ")
+        for j in range(i+1-1):
+            print("$",end=" ")
+        for j in range(i,n-1):
+            print(" ",end=" ")
+        for j in range(i,n-1):
+            print(" ",end=" ")
+        for j in range(i+1-1):
+            print("$",end=" ")
+        for j in range(i+1):
+            print("$",end=" ")
+        print()
+pat(3)                
---
+++ pat2.py
@@ -0,0 +1,23 @@
+def pat(n):
+    
+    for i in range(n - 1):
+        for j in range(i, n):
+            print(" ", end=" ")
+        for j in range(i):
+            print("$", end=" ")
+        for j in range(i + 1):
+            print("$", end=" ")
+        print()   
+
+    
+    for i in range(n):
+         for j in range(i + n):
+             print(" ", end=" ")
+         for j in range(i, n):
+             print("$", end=" ")
+         for j in range(i, n):
+             print("$", end=" ")
+         print()   
+
+
+pat(5)
