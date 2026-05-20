be2bb594ef7e7e1bd67b1f46c78c5000f9d7fc63
Yashwanth R P <yyashwanthrp@gmail.com>
2026-05-20 17:41:12 +0530

    commit5

---
+++ primbrote.py
@@ -0,0 +1,8 @@
+def pri(num):
+    result=[ ]
+    for i in range(1,num+1):
+        if num%i==0:
+            result.append(i)
+    return result
+num=int(input("enter a number:  "))
+print("the prime factors of the number are: ",pri(num))
