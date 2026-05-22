124604cec223b83d3f60e6227ba28f41c208ab3c
Yashwanth R P <yyashwanthrp@gmail.com>
2026-05-23 02:48:18 +0530

    commit7.2

---
+++ patn1.py
@@ -0,0 +1,6 @@
+def pat(n):
+    for i in range (n+1):
+        for j in range(i+1-1):
+            print(i,end=" ")
+        print()
+pat(5)            
---
+++ patn2.py
@@ -0,0 +1,6 @@
+def pat(n):
+    for i in range(n):
+        for j in range(i+1):
+            print(j,end=" ")
+        print()
+pat(5)
