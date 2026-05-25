b2d0e726a3d8cae96f0dd36296f09c87f2318fe5
Yashwanth R P <yyashwanthrp@gmail.com>
2026-05-26 01:08:47 +0530

    commit8.1

---
+++ patn9.py
@@ -0,0 +1,9 @@
+def pat(n):
+    for i in range(n):
+        for j in range(n):
+            if (i==0 or i==n-1):
+                print("$",end=" ")
+            else:
+                print("  ",end=" ")
+        print()
+pat(5)
