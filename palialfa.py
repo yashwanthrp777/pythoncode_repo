458757ebfeb04abf2d3d6cc0da82ec152c1b44fe
Yashwanth R P <yyashwanthrp@gmail.com>
2026-05-29 22:28:00 +0530

    commit11

---
+++ palialfa.py
@@ -0,0 +1,10 @@
+def is_palindrome(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+print(is_palindrome("racecar"))
