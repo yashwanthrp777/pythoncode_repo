03a48c0cdc6df5e3d0796f7d3d3cdcf7e9ddc8b0
Yashwanth R P <yyashwanthrp@gmail.com>
2026-06-01 23:43:46 +0530

    commit12

---
+++ selectionsort.py
@@ -0,0 +1,12 @@
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_index=i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_index]:
+                min_index=j
+        nums[i],nums[min_index]=nums[min_index],nums[i]
+        return nums
+
+nums=[66,32,67,12,87,9]
+print(selection_sort(nums))
