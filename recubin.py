1fdf6b19b7fe5d9bd1a98e6cd825d8b732fe17d8
Yashwanth R P <yyashwanthrp@gmail.com>
2026-07-10 23:49:23 +0530

    commit.46

--- recubin.py
+++ recubin.py
@@ -10,4 +10,4 @@ def fun(nums,low,high):
         return fun(nums,low,mid-1)
 target=int(input("enter the target:  "))
 nums=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]    
-print(f"{fun(nums,0,len(nums)-1)}")
+print(fun(nums,0,len(nums)-1))
